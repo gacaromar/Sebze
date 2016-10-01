688cda322c5d2be3e3f19426fcf4bab38d86998b
Omer <omer-bodur@hotmail.com>
2016-10-01 16:13:21 +0300

    ilk

---
+++ Sebze/App_Start/BundleConfig.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Optimization;
+
+namespace Sebze
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+           // bundles.Add(new ScriptBundle("~/Scripts/"));
+
+           //bundles.Add(new StyleBundle("~/Content/").Include(
+           //     "~/Content/bootstrap.css", "~/Content/css/style.css"
+           //     , "~/Content/css/flexslider.css",
+           //     "~/Content/css/font-awesome.css",
+           //     "~/Content/css/jquery.css",
+           //     "~/Content/css/responsive.css",
+           //     "~/Content/css/revslider.css",
+           //     "~/Content/css/owl.css"));
+        }
+    }
+}
